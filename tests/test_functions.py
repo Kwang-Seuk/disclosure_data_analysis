63aab898ba259aad07bb71665685313ed8199147
Norel <norel.evoagile@gmail.com>
2022-11-04 21:14:59 +0900

    Add support to call function from src folder

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,7 +3,7 @@ sys.path.append("..")
 import pandas as pd
 import numpy as np
 from pandas.testing import assert_frame_equal
-import src.dda.descriptive_statistics as dstat
+from src.dda import descriptive_statistics as dstat
 
 def test_concat_skew_kurt_result_should_return_as_expected():
     data = {
@@ -12,9 +12,10 @@ def test_concat_skew_kurt_result_should_return_as_expected():
     }
 
     df = pd.DataFrame(data)
-    df_descript = df.describe()
-    df_descript.loc['skew'] = df.skew()
-    df_descript.loc['kurt'] = df.kurt()
+    # df_descript = df.describe()
+    # df_descript.loc['skew'] = df.skew()
+    # df_descript.loc['kurt'] = df.kurt()
+    df_descript = dstat(df)
     
     expected = {
         "X1": [4.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0],
