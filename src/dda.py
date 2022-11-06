57ad86fe0e86d7f320d66becdcdf10aa960ff27d
Kwang-Seuk Jeong <kjeong@gsdongju.ac.kr>
2022-11-06 10:46:43 +0900

    "groupby descriptive statistics function added (kurtosis not included)"

--- src/dda.py
+++ src/dda.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from pandas.core.frame import DataFrame
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 def descriptive_statistics(df: DataFrame) -> DataFrame:
     df_descript = df.describe()
@@ -8,3 +11,21 @@ def descriptive_statistics(df: DataFrame) -> DataFrame:
     df_descript.loc["kurt"] = df.kurt()
     return df_descript
 
+
+def descriptive_statistics_groupby(df: DataFrame, group_var: str) -> DataFrame:
+
+    df_grp_descript = (
+        df.groupby(group_var)
+        .agg(["mean", "std", "min", "max", "skew"])
+        .unstack()
+    )
+    return df_grp_descript
+
+
+def mean_std_boxplots(
+    df: DataFrame, rows: int, cols: int, groupby: str
+) -> None:
+
+    fig, ax = plt.subplots(rows, cols, sharey=False)
+    plt.suptitle("")
+    df.boxplot(by=groupby, ax=ax)
