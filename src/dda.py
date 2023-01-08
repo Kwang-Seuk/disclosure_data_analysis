5f15a2334050fdddbeebee62c3ce8da14aa84c67
Kwang-Seuk Jeong <kjeong@gsdongju.ac.kr>
2023-01-08 19:55:41 +0900

    mip data manipulation, result production functions modified

--- src/dda.py
+++ src/dda.py
@@ -1,33 +1,18 @@
 # Loading modules
-#   The belows are the modules used in this function code.
-
-# Fundamental data manipulation
 import os
 import pandas as pd
 import numpy as np
 from pandas.core.frame import DataFrame
 from pandas.core.series import Series
-
-# Illustration
 from matplotlib import pyplot as plt
-
-# ML data preprocessing modules
 from sklearn.model_selection import train_test_split
 from sklearn.inspection import permutation_importance
 from sklearn.model_selection import cross_val_score
 from BorutaShap import BorutaShap
-
-# XGBoost model development
 from xgboost import XGBRegressor, plot_tree
 
 
 # Model development functions
-#   These functions are for development of XGBoost model with data set.
-#   Two seperate functions for input feature selection is prepared: i.e.
-#   forward_seq_feat_selec() and feat_selec_with_borutashap(). Either can be
-#   used in your model development.
-
-
 def load_your_data(
     df: DataFrame, train_size: int, target_var: str
 ) -> DataFrame:
@@ -43,7 +28,7 @@ def load_your_data(
 
 
 def feat_selec_with_borutashap(
-    X_train: DataFrame, X_test: DataFrame, y_train: Series
+    x_train: DataFrame, x_test: DataFrame, y_train: Series, sel_trial: int
 ) -> DataFrame:
 
     # Preliminary XGBoost model creation
@@ -62,9 +47,9 @@ def feat_selec_with_borutashap(
 
     # Fitting and selecting input features
     Feature_Selector.fit(
-        X=X_train,
+        X=x_train,
         y=y_train,
-        n_trials=100,
+        n_trials=sel_trial,
         sample=False,
         train_or_test="train",
         normalize=True,
@@ -73,25 +58,21 @@ def feat_selec_with_borutashap(
 
     # Create new input features sets for training and testing
     features_to_remove = Feature_Selector.features_to_remove
-    X_train_boruta_shap = X_train.drop(columns=features_to_remove)
-    X_test_boruta_shap = X_test.drop(columns=features_to_remove)
+    x_train_sel = x_train.drop(columns=features_to_remove)
+    x_test_sel = x_test.drop(columns=features_to_remove)
 
-    # Sort columns according to column titles
-    X_train_boruta_shap = X_train_boruta_shap.reindex(
-        sorted(X_train_boruta_shap.columns), axis=1
-    )
-    X_test_boruta_shap = X_test_boruta_shap.reindex(
-        sorted(X_test_boruta_shap.columns), axis=1
-    )
+    # Sort input features by column titles
+    x_train_sel = x_train_sel.reindex(sorted(x_train_sel.columns), axis=1)
+    x_test_sel = x_test_sel.reindex(sorted(x_test_sel.columns), axis=1)
 
     print(
         "The number of selected input features is ",
-        len(X_train_boruta_shap.columns),
+        len(x_train_sel.columns),
         "including...",
-        X_train_boruta_shap.columns,
+        str(x_train_sel.columns),
     )
 
-    return X_train_boruta_shap, X_test_boruta_shap
+    return x_train_sel, x_test_sel
 
 
 def hyper_parameters_objective(hpspace: dict):
@@ -112,7 +93,7 @@ def hyper_parameters_objective(hpspace: dict):
     )
     best_score = cross_val_score(
         xgb_hpo,
-        hpspace["X_train"],
+        hpspace["x_train"],
         hpspace["y_train"],
         scoring="neg_mean_squared_error",
         cv=10,
@@ -121,30 +102,27 @@ def hyper_parameters_objective(hpspace: dict):
     return loss
 
 
-def develop_your_production_model(
-    hpspace: dict,
-    best: dict,
-):
+def develop_production_model(data_dict: dict, best: dict):
 
-    X_train = hpspace["X_train"]
-    y_train = hpspace["y_train"]
-    X_test = hpspace["X_test"]
-    y_test = hpspace["y_test"]
+    x_train = data_dict["x_train"]
+    x_test = data_dict["x_test"]
+    y_train = data_dict["y_train"]
+    y_test = data_dict["y_test"]
 
     best["max_depth"] = int(best["max_depth"])
 
     xgb_model_production = XGBRegressor(
         objective="reg:squarederror",
         n_estimators=1000,
-        n_jobs=-1,
+        n_jobs=1,
         **best,
         tree_method="gpu_hist",
     )
 
     xgb_model_production.fit(
-        X_train,
+        x_train,
         y_train,
-        eval_set=[(X_train, y_train), (X_test, y_test)],
+        eval_set=[(x_train, y_train), (x_test, y_test)],
         eval_metric=["rmse"],
         verbose=100,
         early_stopping_rounds=400,
@@ -154,16 +132,8 @@ def develop_your_production_model(
 
 
 # Training results illustration functions
-#   The followings enable you to investigate the trained production model
-#   structure and its predictability against training and testing data sets,
-#   by depicting the model output results
+def best_tree_illustration(model, fig_dpi: int, fig_save: bool = True):
 
-
-def best_tree_illustration(
-    model, fig_size: tuple, fig_dpi: int, fig_save: bool = True
-):
-
-    plt.rcParams["figure.figsize"] = fig_size
     plt.rcParams["figure.dpi"] = fig_dpi
     plot_tree(
         model,
@@ -171,53 +141,62 @@ def best_tree_illustration(
     )
     if fig_save is True:
         cwd = os.getcwd()
-        plt.savefig(cwd + "/fig_test_tree.jpg", dpi="figure")
+        plt.savefig(cwd + "/fig_test_tree.jpg", dpi=fig_dpi)
 
 
-def predict_plot_train_test(model, hpspace: dict, fig_save: bool = True):
+def predict_plot_train_test(
+    model, data_dict: dict, fig_dpi: int, fig_save: bool = True
+):
 
-    X_train = hpspace["X_train"]
-    y_train = hpspace["y_train"]
-    X_test = hpspace["X_test"]
-    y_test = hpspace["y_test"]
+    plt.rcParams["figure.dpi"] = fig_dpi
 
-    tr_pred = model.predict(X_train)
-    plt.rcParams["figure.figsize"] = (15, 15)
+    x_train = data_dict["x_train"]
+    x_test = data_dict["x_test"]
+    y_train = data_dict["y_train"]
+    y_test = data_dict["y_test"]
+
+    tr_pred = model.predict(x_train)
     plt.scatter(y_train, tr_pred)
 
-    tst_pred = model.predict(X_test)
-    plt.rcParams["figure.figsize"] = (15, 15)
+    tst_pred = model.predict(x_test)
     plt.scatter(y_test, tst_pred)
 
     if fig_save is True:
         cwd = os.getcwd()
         plt.savefig(
-            cwd + "fig_train_test_prediction_results.png", dpi="figure"
+            cwd + "/fig_train_test_prediction_results.png", dpi=fig_dpi
         )
 
 
-def feat_importance_general(model, hpspace: dict, fig_save: bool = True):
-    X_train = hpspace["X_train"]
-    model.feature_importances_
-    feature_names = np.array(X_train.columns)
+def feat_importance_general(
+    model, data_dict: dict, fig_dpi: int, fig_save: bool = True
+):
+
+    plt.rcParams["figure.dpi"] = fig_dpi
+
+    x_train = data_dict["x_train"]
+    feature_names = np.array(x_train.columns)
     sorted_idx = model.feature_importances_.argsort()
-    plt.figure(figsize=(15, 10))
     plt.barh(feature_names[sorted_idx], model.feature_importances_[sorted_idx])
     plt.xlabel("XGBoost Feature Importance")
 
     if fig_save is True:
         cwd = os.getcwd()
-        plt.savefig(cwd + "fig_feature_importance.png", dpi="figure")
+        plt.savefig(cwd + "/fig_feature_importance.png", dpi=fig_dpi)
 
 
-def feat_importance_permut(model, hpspace: dict, fig_save: bool = True):
-    X_train = hpspace["X_train"]
-    y_train = hpspace["y_train"]
+def feat_importance_permut(
+    model, data_dict: dict, fig_dpi: int, fig_save: bool = True
+):
 
-    perm_importance = permutation_importance(model, X_train, y_train)
-    feature_names = np.array(X_train.columns)
+    plt.rcParams["figure.dpi"] = fig_dpi
+
+    x_train = data_dict["x_train"]
+    y_train = data_dict["y_train"]
+
+    perm_importance = permutation_importance(model, x_train, y_train)
+    feature_names = np.array(x_train.columns)
     sorted_idx = perm_importance.importances_mean.argsort()
-    plt.figure(figsize=(15, 10))
     plt.barh(
         feature_names[sorted_idx], perm_importance.importances_mean[sorted_idx]
     )
@@ -226,27 +205,20 @@ def feat_importance_permut(model, hpspace: dict, fig_save: bool = True):
     if fig_save is True:
         cwd = os.getcwd()
         plt.savefig(
-            cwd + "fig_feature_importance_permuted.png",
-            dpi="figure",
+            cwd + "/fig_feature_importance_permuted.png",
+            dpi=fig_dpi,
         )
 
 
 # Most-influencing parameter
-#   The functions generate min-max ranged linear values for every input feature
-#   selected by the modelling process. A dataframe consisting of One linearly
-#   varying feature and average values for the remaining features is generated
-#   with the following functions. Then you can campare  responses of the trained
-#   model against the given gradual variation of each input feature.
+def create_interpolation_for_mip(data_dict: dict, interval: int) -> DataFrame:
 
+    x_train = data_dict["x_train"]
 
-def min_max_linspace_for_mip(hpspace: dict, interval: int) -> DataFrame:
-
-    X_train = hpspace["X_train"]
-
-    minmax_df = pd.DataFrame(X_train.nunique(), columns=["nunique"])
-    minmax_df["min"] = X_train.min()
-    minmax_df["max"] = X_train.max()
-    minmax_df["dtypes"] = X_train.dtypes
+    minmax_df = pd.DataFrame(x_train.nunique(), columns=["nunique"])
+    minmax_df["min"] = x_train.min()
+    minmax_df["max"] = x_train.max()
+    minmax_df["dtypes"] = x_train.dtypes
 
     df_interpolated = pd.DataFrame()
     for _, col_name in enumerate(minmax_df.index):
@@ -257,168 +229,61 @@ def min_max_linspace_for_mip(hpspace: dict, interval: int) -> DataFrame:
     return df_interpolated
 
 
-def create_df_mip_with_means_and_itp_data(
-    hpspace: dict, df_interpolated: DataFrame
+def create_df_means_for_mip(
+    data_dict: dict, df_interpolated: DataFrame
 ) -> DataFrame:
 
-    X_train = hpspace["X_train"]
+    x_train = data_dict["x_train"]
 
-    df_mean_tmp = pd.DataFrame(X_train.mean(), columns=["mean"])
+    df_mean_tmp = pd.DataFrame(x_train.mean(), columns=["mean"])
     df_means = pd.DataFrame(
-        columns=list(X_train.columns), index=range(len(df_interpolated))
+        columns=list(x_train.columns), index=range(len(df_interpolated))
     )
     for col_name in df_mean_tmp.index:
         df_means[col_name] = df_mean_tmp["mean"][col_name]
-    df_means = df_means.add_suffix("_means")
-    df_mip = df_interpolated.join(df_means)
-    return df_mip
+    return df_means
 
 
-def run_mip_analysis_with_df_mip(df_mip: DataFrame, model: str, data_dir: str):
-
-    X_grp_no = int(len(df_mip.columns) / 2)
-    X_itp = df_mip.iloc[:, 0:X_grp_no]
-    X_means = df_mip.iloc[:, X_grp_no:]
+def mip_analysis(
+    df_means: DataFrame, df_itp: DataFrame, model: str, save_res: bool = True
+):
 
     # store data for further plot creation
     df_mip_res = pd.DataFrame()
-    # X_itp_for_plot = X_itp.copy()
+    df_means_copy = df_means.copy()
 
-    for i in range(X_grp_no):
-        # make an mip data set for prediction
-        X_itp_tmp = X_itp.copy()
-        X_means_tmp = X_means.copy()
-        X_mip = X_means_tmp.drop(X_means_tmp.columns[i], axis=1)
-        X_mip.columns = X_mip.columns.str.replace("_means", "")
-        X_itp_series = X_itp_tmp.iloc[:, i]
-        X_mip = X_mip.merge(
-            X_itp_series.to_frame(), left_index=True, right_index=True
-        )
+    for col_name in df_means.columns:
+        df_means_copy[col_name] = df_itp[col_name]
+        mip_pred = model.predict(df_means_copy)
+        df_mip_res[col_name] = mip_pred.tolist()
 
-        # model prediction with mip data, store the result to df_mip_res
-        mip_pred = model.predict(X_mip)
-        df_mip_res[mip_col_name] = mip_pred.tolist()
-
-        # save the X_mip as a csv file with its itp column name
-        mip_col_name = X_mip.columns[-1]
-        X_mip["res"] = mip_pred.tolist()
-        X_mip.to_csv(data_dir + "x_mip_" + mip_col_name + ".csv")
-        # vars()["X_mip_" + mip_col_name] = X_mip
+    # save the mip results as a csv file
+    if save_res is True:
+        cwd = os.getcwd()
+        df_mip_res.to_csv(cwd + "/mip_analysis_res" + ".csv")
 
     return df_mip_res
 
 
-def plot_mip_analysis_results(df_mip_input: DataFrame, df_mip_res: DataFrame):
-
-    no_input_feat = len(df_mip_input.columns)
-    params = {
-        "font.size": 13.0,
-        "axes.titlesize": "medium",
-        "figure.figsize": (15, 10),
-        "axes.grid": True,
-        # "figure.dpi": 75
-    }
-    plt.rcParams.update(params)
-
-    col_list = list(df_mip_input.columns)
-    subplot_titles = [f"{col}" for col in col_list]
-
-    fig = plt.figure()
-    for i in range(no_input_feat):
-        ax = fig.add_subplot(5, 5, 1 + i)
-        ax.plot(df_mip_input.iloc[:, i], df_mip_res.iloc[:, i], "r-")
-        ax.set_title(subplot_titles[i])
-    fig.tight_layout()
-
-
-# Randomized simulation functions
-#    These functions are designed (1) to create min-max interpolated data
-#    for every input feature, and (2) to generate randomized data blocks
-#    consisted of the interpolated data and randomly distributed data.
-
-
-def minmax_table(df: DataFrame, rdn_num: int):
-
-    minmax_df = pd.DataFrame(df.nunique(), columns=["nunique"])
-    minmax_df["max"] = df.max()
-    minmax_df["min"] = df.min()
-    minmax_df["dtypes"] = df.dtypes
-
-    df_rdn = pd.DataFrame()
-
-    for col_name in minmax_df.index:
-        if minmax_df["dtypes"][col_name] == "float64":
-            df_rdn[col_name] = (
-                np.random.rand(rdn_num)
-                * (minmax_df["max"][col_name] - minmax_df["min"][col_name])
-                + minmax_df["min"][col_name]
-            )
-        else:
-            df_rdn[col_name] = np.random.randint(
-                minmax_df["min"][col_name],
-                minmax_df["max"][col_name] + 1,
-                size=rdn_num,
-            )
-
-    return minmax_df, df_rdn
-
-
-def rdn_simul_data_create(
-    minmax_df: DataFrame,
-    df_rdn: DataFrame,
-    linnum: int,
-    print_option: str,
-    out_dir: str,
+def plot_mip_analysis_results(
+    df_itp: DataFrame,
+    df_mip_res: DataFrame,
+    fig_dpi: int,
+    fig_save: bool = True,
 ):
 
-    df_tmp = pd.DataFrame()
+    col_list = list(df_itp.columns)
 
-    for idx, col_name in enumerate(minmax_df.index):
+    fig = plt.figure()
+    for i, col_name in enumerate(col_list):
+        ax = fig.add_subplot(10, 5, i + 1)
+        ax.plot(df_itp[col_name], df_mip_res[col_name], "r-")
+        ax.set_title(col_name)
+        fig.tight_layout()
 
-        # If the number of unique values of a variable is > linnum,
-        # make values equals to linnum, otherwise number of values = nunique
-        if minmax_df["nunique"][col_name] > linnum:
-            temp_linspace = np.linspace(
-                minmax_df["min"][col_name], minmax_df["max"][col_name], linnum
-            )
-        else:
-            temp_linspace = np.linspace(
-                minmax_df["min"][col_name],
-                minmax_df["max"][col_name],
-                minmax_df["nunique"][col_name],
-            )
-
-        temp_name = "df_rdn_" + col_name
-        globals()[temp_name] = pd.DataFrame()
-
-        for idx, value in enumerate(temp_linspace):
-            df_temp = df_rdn.copy()
-            df_temp[col_name] = value
-
-            if idx == 0:
-                temp_name = df_temp.copy()
-            else:
-                temp_name = temp_name.append(df_temp)
-
-        # if col_name == 'Male':
-        #  temp_name['Female'] =  np.where( temp_name['Male'] ==0, 1, 0)
-        # if col_name == 'Female':
-        #  temp_name['Male'] =  np.where( temp_name['Female'] ==0, 1, 0)
-
-        if idx == 0:
-            df_tmp = temp_name.copy()
-        else:
-            df_tmp = df_tmp.append(temp_name)
-
-        # Make CSV files
-        print(
-            "df_rdn_"
-            + col_name
-            + " done. "
-            + str(temp_name.shape[0])
-            + " rows"
+    if fig_save is True:
+        cwd = os.getcwd()
+        plt.savefig(
+            cwd + "/fig_mip_results.png",
+            dpi=fig_dpi,
         )
-        # print(temp_name.head())
-        if print_option is True:
-            # temp_name.to_excel(out_data_dir + "df_rdn_" + col_name + ".xlsx")
-            temp_name.to_csv(out_dir + "df_rdn_" + col_name + ".csv")
--- src/run_dda.py
+++ src/run_dda.py
@@ -1,116 +1,78 @@
 # Loading modules
-#   The following codes allow you to load necessary modules for
-#   the analysis.
-
-# Data manipulation modules
 import pandas as pd
 import numpy as np
-
-# Model development modules
 from hyperopt import hp, fmin, tpe
 from src.dda import (
     load_your_data,
     feat_selec_with_borutashap,
     hyper_parameters_objective,
-    develop_your_production_model,
+    develop_production_model,
     best_tree_illustration,
     feat_importance_general,
     feat_importance_permut,
-    min_max_linspace_for_mip,
-    create_df_mip_with_means_and_itp_data,
-    run_mip_analysis_with_df_mip,
+    create_interpolation_for_mip,
+    create_df_means_for_mip,
+    mip_analysis,
     plot_mip_analysis_results,
-    minmax_table,
-    rdn_simul_data_create,
     predict_plot_train_test,
 )
 
 
-# Load data and data preparation for modelling
-#    The functions below load your input data and slit it into
-#    four subsets of data: i.e. input/output and training/testing.
-
+# Load data and input feature selection for modelling
 data_dir = "src/"
 data_file = "rawdata_analysis_employment.csv"
 
 df = pd.read_csv(data_dir + data_file, index_col=[0, 1, 2, 3])
-X_train, X_test, y_train, y_test = load_your_data(df, 268, "Employment_rates")
+x_train, x_test, y_train, y_test = load_your_data(df, 268, "Employment_rates")
 
-
-# Input feature seleciton (BorutaShap) & hyper-parameter optimization
-#    The data prepared above flow to the next step here. Tho following
-#    functions distinquish input features that affect more greatly
-#    to predictability from redundant features. Then the selected
-#    input features are used to optimize XGBoost model hyper-parameters.
-
-# Input feature selection: using BorutaShap module
-
-X_train_boruta_shap, X_test_boruta_shap = feat_selec_with_borutashap(
-    X_train, X_test, y_train
+x_train_boruta_shap, x_test_boruta_shap = feat_selec_with_borutashap(
+    x_train, x_test, y_train, 500
 )
 
 # Hyper-parameters optimization
-
 hpspace = {
     "max_depth": hp.quniform("max_depth", 3, 10, 1),
-    "gamma": hp.uniform("gamma", 1, 9),
+    "gamma": hp.uniform("gamma", 0, 10),
     "reg_alpha": hp.quniform("reg_alpha", 0, 0.5, 0.1),
     "reg_lambda": hp.uniform("reg_lambda", 0, 1),
-    "eta": hp.uniform("eta", 0.01, 0.2),
+    "eta": hp.uniform("eta", 0, 1),
     "min_child_weight": hp.quniform("min_child_weight", 0, 2, 0.1),
     "subsample": hp.uniform("subsample", 0.5, 1),
     "colsample_bytree": hp.uniform("colsample_bytree", 0.1, 1),
     "scale_pos_weight": hp.uniform("scale_pos_weight", 0.1, 1),
-    "X_train": X_train_boruta_shap,
+    "x_train": x_train_boruta_shap,
     "y_train": y_train,
-    "X_test": X_test_boruta_shap,
-    "y_test": y_test,
 }
 
 best = fmin(
     fn=hyper_parameters_objective,
     space=hpspace,
-    max_evals=10,
+    max_evals=5,
     rstate=np.random.default_rng(777),
     algo=tpe.suggest,
 )
 print(best)
 
-
 # Production model development
-#    This section aims at making a new XGBoost model using previsouly
-#    created "selected input feature" data and optimized hyper-parameters
-#    setting.
-
-xgb_production_model = develop_your_production_model(hpspace, best)
+input_data_dict = {
+    "x_train": x_train_boruta_shap,
+    "x_test": x_test_boruta_shap,
+    "y_train": y_train,
+    "y_test": y_test,
+}
+xgb_production_model = develop_production_model(input_data_dict, best)
 
 # Depicting modelling results
-#    Here you can see the shape of best tree model consisting of
-#    the ensemble models, as well as prediction results and input
-#    feature importance.
-
-best_tree_illustration(xgb_production_model, (15, 15), 150, True)
-predict_plot_train_test(xgb_production_model, hpspace, True)
-feat_importance_general(xgb_production_model, hpspace, True)
-feat_importance_permut(xgb_production_model, hpspace, True)
+best_tree_illustration(xgb_production_model, 150, False)
+predict_plot_train_test(xgb_production_model, input_data_dict, 150, False)
+feat_importance_general(xgb_production_model, input_data_dict, 150, False)
+feat_importance_permut(xgb_production_model, input_data_dict, 150, False)
 
 
 # Simulation (1): Most Influencing Parameters (MIP) analysis
-#    The functions below execute simple simulation, to confirm
-#    how the output variable respond to linear changes of one
-#    selected input feature. The remaining input features are
-#    fixed at average of each.
-
-df_interpolated = min_max_linspace_for_mip(X_train_boruta_shap, 21)
-df_mip = create_df_mip_with_means_and_itp_data(
-    X_train_boruta_shap, df_interpolated
+df_itp_employ = create_interpolation_for_mip(input_data_dict, 21)
+df_means_employ = create_df_means_for_mip(input_data_dict, df_itp_employ)
+df_mip_res_employ = mip_analysis(
+    df_means_employ, df_itp_employ, xgb_production_model, True
 )
-df_mip_input, df_mip_res = run_mip_analysis_with_df_mip(
-    df_mip, xgb_production_model, data_dir
-)
-plot_mip_analysis_results(df_mip_input, df_mip_res)
-
-## Random simulation for input features
-
-minmax_df, df_rdn = minmax_table(X_train_boruta_shap, 11)
-rdn_simul_data_create(X_train_boruta_shap, minmax_df)
+plot_mip_analysis_results(df_itp_employ, df_mip_res_employ, 150, False)
