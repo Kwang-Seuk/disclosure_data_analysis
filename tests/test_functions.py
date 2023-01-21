256df0fff0184e4ef8298cd718a86027cb61f185
Kwang-Seuk Jeong <kjeong@gsdongju.ac.kr>
2023-01-22 02:37:51 +0900

    test code completed for first distribution (random simulation removed)

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,11 +1,58 @@
+from statistics import mean
+import math
 import pandas as pd
+import numpy as np
+from hyperopt import hp, fmin, tpe
+from xgboost import XGBRegressor
 from pandas.testing import assert_frame_equal, assert_series_equal
+import pytest
+from scipy import stats
+from fitter import Fitter
 from src.dda import (
     load_your_data,
+    feat_selec_with_borutashap,
+    hyper_parameters_objective,
+    develop_production_model,
     create_interpolation_for_mip,
     create_df_means_for_mip,
-    minmax_table,
+    create_minmax_df,
+    create_rdn_df,
+    rdn_simul_data_create,
 )
+
+
+@pytest.fixture(scope="module")
+def fixture_input_df():
+    x1 = np.random.standard_normal(1000)
+    x2 = np.random.standard_normal(1000) * 1.5
+    x3 = [i * i for i in x2]
+    x4 = [i / j for i, j in zip(x1, x2)]
+    x5 = [i * j for i, j in zip(x1, x2)]
+    y = [i / (i * j) for i, j in zip(x1, x2)]
+
+    df = pd.DataFrame(
+        list(zip(x1, x2, x3, x4, x5, y)),
+        columns=["x1", "x2", "x3", "x4", "x5", "y"],
+    )
+
+    return df
+
+
+@pytest.fixture(scope="module")
+def fixture_hpspace():
+    hpspace = {
+        "max_depth": hp.quniform("max_depth", 3, 10, 1),
+        "gamma": hp.uniform("gamma", 0, 10),
+        "reg_alpha": hp.quniform("reg_alpha", 0, 0.5, 0.1),
+        "reg_lambda": hp.uniform("reg_lambda", 0, 1),
+        "eta": hp.uniform("eta", 0, 1),
+        "min_child_weight": hp.quniform("min_child_weight", 0, 2, 0.1),
+        "subsample": hp.uniform("subsample", 0.5, 1),
+        "colsample_bytree": hp.uniform("colsample_bytree", 0.1, 1),
+        "scale_pos_weight": hp.uniform("scale_pos_weight", 0.1, 1),
+    }
+
+    return hpspace
 
 
 def test_data_loading_should_return_expected():
@@ -17,31 +64,110 @@ def test_data_loading_should_return_expected():
     }
 
     df = pd.DataFrame(data)
-    X_train, X_test, y_train, y_test = load_your_data(df, 3, "y")
+    x_train, x_test, y_train, y_test = load_your_data(df, 3, "y")
 
-    X_test = X_test.reset_index(drop=True)
+    x_test = x_test.reset_index(drop=True)
     y_test = y_test.reset_index(drop=True)
 
-    expected_X_train = {
+    expected_x_train = {
         "x1": [1, 1, 1],
         "x2": [2, 2, 2],
     }
-    expected_X_test = {
+    expected_x_test = {
         "x1": [10, 10],
         "x2": [20, 20],
     }
 
-    df_expected_X_train = pd.DataFrame(expected_X_train)
-    df_expected_X_test = pd.DataFrame(expected_X_test)
+    df_expected_x_train = pd.DataFrame(expected_x_train)
+    df_expected_x_test = pd.DataFrame(expected_x_test)
 
     ser_expected_y_train = pd.Series([3, 3, 3], dtype="int64", name="y")
     ser_expected_y_test = pd.Series([30, 30], dtype="int64", name="y")
 
-    assert_frame_equal(X_train, df_expected_X_train)
-    assert_frame_equal(X_test, df_expected_X_test)
+    assert_frame_equal(x_train, df_expected_x_train)
+    assert_frame_equal(x_test, df_expected_x_test)
 
     assert_series_equal(y_train, ser_expected_y_train)
     assert_series_equal(y_test, ser_expected_y_test)
+
+
+def test_selected_feats_no_should_be_smaller_than_raw_input_feats(
+    fixture_input_df,
+):
+
+    x_train, x_test, y_train, y_test = load_your_data(
+        fixture_input_df, 600, "y"
+    )
+
+    x_selec_train, x_selec_test = feat_selec_with_borutashap(
+        x_train, x_test, y_train, 50
+    )
+
+    assert len(x_train.columns) >= len(x_selec_train.columns)
+    assert len(x_test.columns) >= len(x_selec_test.columns)
+
+
+def test_hyperparameters_opt_model_works_better_than_normal(
+    fixture_input_df, fixture_hpspace
+):
+    x_train, x_test, y_train, y_test = load_your_data(
+        fixture_input_df, 600, "y"
+    )
+    x_selec_train, x_selec_test = feat_selec_with_borutashap(
+        x_train, x_test, y_train, 50
+    )
+
+    # Preliminary xgboost model training & final rmse obtainment
+    xgb_pre = XGBRegressor(
+        objective="reg:squarederror",
+        n_estimators=1000,
+        n_jobs=1,
+        tree_method="gpu_hist",
+    )
+
+    xgb_pre.fit(
+        x_selec_train,
+        y_train,
+        eval_set=[(x_selec_train, y_train), (x_selec_test, y_test)],
+        eval_metric=["rmse"],
+        verbose=100,
+        early_stopping_rounds=400,
+    )
+    xgb_pre_result = xgb_pre.evals_result()
+    xgb_pre_train_fin_rmse = xgb_pre_result["validation_0"]["rmse"][-1]
+    xgb_pre_test_fin_rmse = xgb_pre_result["validation_1"]["rmse"][-1]
+
+    # Production xgboost model training & final rmse obtainment
+    fixture_hpspace["x_train"] = x_selec_train
+    fixture_hpspace["y_train"] = y_train
+
+    best = fmin(
+        fn=hyper_parameters_objective,
+        space=fixture_hpspace,
+        max_evals=50,
+        rstate=np.random.default_rng(777),
+        algo=tpe.suggest,
+    )
+
+    input_dict = {
+        "x_train": x_selec_train,
+        "x_test": x_selec_test,
+        "y_train": y_train,
+        "y_test": y_test,
+    }
+
+    xgb_production = develop_production_model(input_dict, 1000, best)
+
+    xgb_production_result = xgb_production.evals_result()
+    xgb_production_train_fin_rmse = xgb_production_result["validation_0"][
+        "rmse"
+    ][-1]
+    xgb_production_test_fin_rmse = xgb_production_result["validation_1"][
+        "rmse"
+    ][-1]
+
+    assert xgb_production_train_fin_rmse <= xgb_pre_train_fin_rmse
+    assert xgb_production_test_fin_rmse <= xgb_pre_test_fin_rmse
 
 
 def test_interpolation_for_mip_should_result_as_expected():
@@ -92,7 +218,6 @@ def test_interpolation_for_mip_should_result_as_expected():
 
 
 def test_create_means_for_mip_should_result_as_expected():
-
     data_x_train = {
         "X1": [1, 2, 3, 4, 5, 6],
         "X2": [4, 5, 6, 7, 8, 9],
@@ -120,49 +245,3 @@ def test_create_means_for_mip_should_result_as_expected():
 
     assert_frame_equal(df_itp, df_expected_itp)
     assert_frame_equal(df_means, df_expected_means)
-
-
-def test_minmax_table_function_should_result_as_expected():
-
-    data = {
-        "X1": [1, 2, 3, 4],
-        "X2": [4, 5, 6, 7],
-        "X3": [7, 7, 8, 8],
-    }
-    df = pd.DataFrame(data)
-    data_dict = {"x_train": df}
-    minmax_df, df_rdn = minmax_table(data_dict, 5)
-
-    minmax_data_expected = {
-        "index": ["X1", "X2", "X3"],
-        "nunique": [4, 4, 2],
-        "max": [4, 7, 8],
-        "min": [1, 4, 7],
-        "dtypes": ["int64", "int64", "int64"],
-    }
-
-    df_minmax_data_expected = pd.DataFrame(minmax_data_expected)
-    df_minmax_data_expected.set_index("index", inplace=True)
-    df_minmax_data_expected.index.name = None
-
-    assert_frame_equal(minmax_df, df_minmax_data_expected)
-
-    ser_df_rdn_count = df_rdn.count()
-    expected_rdn_count = {"X1": 5, "X2": 5, "X3": 5}
-    ser_expected_rdn_count = pd.Series(expected_rdn_count)
-
-    assert_series_equal(ser_df_rdn_count, ser_expected_rdn_count)
-
-    ser_df_rdn_min = df_rdn.min()
-    expected_rdn_min = {"X1": 1, "X2": 4, "X3": 7}
-
-    assert ser_df_rdn_min[0] >= expected_rdn_min["X1"]
-    assert ser_df_rdn_min[1] >= expected_rdn_min["X2"]
-    assert ser_df_rdn_min[2] >= expected_rdn_min["X3"]
-
-    ser_df_rdn_max = df_rdn.max()
-    expected_rdn_max = {"X1": 4, "X2": 7, "X3": 8}
-
-    assert ser_df_rdn_max[0] <= expected_rdn_max["X1"]
-    assert ser_df_rdn_max[1] <= expected_rdn_max["X2"]
-    assert ser_df_rdn_max[2] <= expected_rdn_max["X3"]
